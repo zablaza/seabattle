b4397568ec4b9f6c0a4b0af8a541dfd9b076e00c
zablaza <katrenuknazar@gmail.com>
2023-12-10 09:57:51 +0200

    created application for battleground

--- app/game/views.py
+++ app/game/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
 
 def game(request):
-    return HttpResponse("Hello, this is sea battle")
+    height1 = 11
+    lenght1 = 11
+    start_state = []
+    for a in range(1, height1):
+        c = []
+        for b in range(1, lenght1):
+            c.append({'display_value': ' ', 'index': str((a - 1) * (lenght1 - 1) + (b - 1))})
+        start_state.append(c)
+    state = start_state
+    print(state)
+    c={}
+    return render(request=request, template_name="game/gamefield.html", context={"field": state})
