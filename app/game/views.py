bae2e9c57f5cdf7461b1a2156b35d1647bf542f2
zablaza <katrenuknazar@gmail.com>
2024-01-21 11:53:04 +0200

    canvas coordinates

--- app/game/views.py
+++ app/game/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
-# Create your views here.
 
 def game(request):
-    height1 = 11
-    lenght1 = 11
+    height = 11
+    length = 11
     start_state = []
-    for a in range(1, height1):
-        c = []
-        for b in range(1, lenght1):
-            c.append({'display_value': ' ', 'index': str((a - 1) * (lenght1 - 1) + (b - 1))})
-        start_state.append(c)
+
+    for row in range(height):
+        row_content = []
+        for col in range(length):
+            if row > 0 and col > 0:
+                row_content.append({'display_value': ' ', 'index': str((row - 1) * (length - 1) + (col - 1))})
+            elif row > 0 and col == 0:
+                row_content.append({'display_value': row, 'index': str((row - 1) * (length - 1) + (col - 1))})
+            else:
+                row_content.append({'display_value': ' ', 'index': "x"})
+        start_state.append(row_content)
     state = start_state
+
+    alphabet = [' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    for index, value in enumerate(state[0]):
+        value['display_value'] = alphabet[index]
+
     print(state)
-    c={}
-    return render(request=request, template_name="game/gamefield.html", context={"field": state})
+
+    return render(request=request, template_name="game/gamefield.html", context={"field": state, "alphabet": ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'], "numbers": [0,1,2,3,4,5,6,7,8,9]})
